85ad9caf9f54268b1119a17cd3183cec6d95616f
nick <nick.feng@chinaredstar.com>
2016-07-29 13:14:54 +0800

    dfdf

--- tools/config/anjuke/index.js
+++ tools/config/anjuke/index.js
@@ -5,7 +5,7 @@
 let _ = require("lodash");
 
 module.exports = (core) => {
-    let config = new core.utils.builder("anjuke", "www.anjuke.com", ["sh.fang.anjuke.com", "shanghai.anjuke.com"]);
+    let config = new core.utils.builder("anjuke", "www.anjuke.com", ["sh.fang.anjuke.com", "shanghai.anjuke.com", "dulijingjiren11.anjuke.com", "jiaxinfangchan.anjuke.com", "hengtongdichan.anjuke.com", "hanchangfangcha1.anjuke.com", "huanxindichan.anjuke.com", "shenzhoufangcha.anjuke.com", "zhushangbudongc.anjuke.com", "woaiwojia19.anjuke.com", "changyuandichan.anjuke.com", "fumeilai.anjuke.com"]);
 
     config.setBaseInfo(1000, "superagent");
     config.initDomain = "shanghai.anjuke.com";
@@ -18,32 +18,13 @@ module.exports = (core) => {
         }
     };
     // 白名单
-    // 匹配新房列表
-    config.addWhitePath(/^\/loupan\/all\/p\d+/);
-    // 匹配新房主页
-    config.addWhitePath(/^\/loupan\/\d+\.html/);
-    // 匹配新房参数页面
-    config.addWhitePath(/^\/loupan\/canshu-\d+\.html/);
 
-    // 匹配新房户型参数页面
-    config.addWhitePath(/^\/loupan\/huxing-\d+\.html/);
-    // 户型分页
-    config.addWhitePath(/^\/loupan\/huxing-\d+\/s/);
-    // 匹配新房相册页面
-    config.addWhitePath(/^\/loupan\/xiangce-\d+/);
-
-
-
-    // 小区户型图
-    config.addWhitePath(/^\/community\/photos\/model\/\d+/);
-    // 匹配小区列表页，小区详情页（只根据区域版块过滤）
-    config.addWhitePath(/^\/community(?:\/?[a-z]*\/?(?:p\d+\/?|\/?)|\/?|\/view\/\d+)$/);
-
-    // 匹配经纪人列表页面
-    config.addWhitePath(/^\/tycoon\//);
-    config.addWhitePath(/^\/tycoon\/p\d+\//);
-    // 匹配经纪人详情
-    config.addWhitePath(/^\/gongsi-jjr-\d+\/(js)?/);
+    // 楼盘列表、分页、相册,户型
+    config.addWhitePath(/^\/loupan(?:\/?|\/(?:[a-z]*(?:\/p\d+\/?|\/?))|\/(?:canshu|huxing|xiangce|)-?\d+\/?(?:ybj|sjt|xgt|ght|ptt|wzt|)\.html)$/);
+    // 匹配小区列表页，小区详情页,户型图页（只根据区域版块过滤）
+    config.addWhitePath(/^\/community\/?(?:[a-z]*(?:\/p\d+\/?|\/?)|\/view\/\d+|photos\/model\/\d+)\/?$/);
+    // 经纪人列表,经纪人详情,经纪人个人信息
+    config.addWhitePath(/^(?:\/tycoon\/?(?:[a-z]*(?:\/?p\d+|)|)|\/gongsi-jjr-\d+(\/js)?)\/?$/);
 
     _.forEach(core.config.anjuke.pages, (page) => {
         if (typeof page === "function") {
