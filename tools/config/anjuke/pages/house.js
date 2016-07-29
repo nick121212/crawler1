265ff08d27a5b0f21384cc98a10de60f5c282cac
nick <nick.feng@chinaredstar.com>
2016-07-29 13:42:41 +0800

    dfdf

--- tools/config/anjuke/pages/house.js
+++ tools/config/anjuke/pages/house.js
@@ -70,63 +70,84 @@ module.exports = (core) => {
                     ]
                 },
                 sellInfo: {
-                    selector: ".can-container .can-item:eq(1) .can-border .list",
+                    selector: ".can-container .can-item:eq(1) .can-border .list li",
                     dealStrategy: "jsdom",
                     data: [
-                        // 最低首付
-                        core.utils.data_builder.normal("downPayment", ["li:eq(0) .des"], [".space"]),
-                        // 月供
-                        core.utils.data_builder.normal("monthPayment", ["li:eq(1) .des"]),
-                        // 开盘时间
-                        core.utils.data_builder.normal("startTime", ["li:eq(3) .des"]),
-                        // 交房时间
-                        core.utils.data_builder.normal("deliveryTime", ["li:eq(4) .des"]),
-                        // 售楼处地址
-                        core.utils.data_builder.normal("sellAddress", ["li:eq(5) .des"])
+                        core.utils.data_builder.switchs([], [], [
+                            core.utils.data_builder.cases(".name", [], "最低首付", [
+                                core.utils.data_builder.normal("downPayment", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "开盘时间", [
+                                core.utils.data_builder.normal("startTime", [".des"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "交房时间", [
+                                core.utils.data_builder.normal("deliveryTime", [".des"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "售楼处地址", [
+                                core.utils.data_builder.normal("sellAddress", [".des"])
+                            ])
+                        ])
                     ]
                 },
                 communityInfo: {
-                    selector: ".can-container .can-item:eq(2) .can-border .list",
+                    selector: ".can-container .can-item:eq(2) .can-border .list li",
                     dealStrategy: "jsdom",
                     data: [
-                        // 建筑类型
-                        core.utils.data_builder.normal("buildingType", ["li:eq(0) .des"], [".space"]),
-                        // 产权年限
-                        core.utils.data_builder.normal("propertyAge", ["li:eq(1) .des"], [".space"]),
-                        // 装修标准
-                        core.utils.data_builder.normal("decorationStandard", ["li:eq(2) .des"], [".space"]),
-                        // 容积率
-                        core.utils.data_builder.normal("volumeRate", ["li:eq(3) .des"], [".space"]),
-                        // 绿化率
-                        core.utils.data_builder.normal("greeningRate", ["li:eq(4) .des"], [".space"]),
-                        // 规划户数
-                        core.utils.data_builder.normal("totalHouseholds", ["li:eq(5) .des"], [".space"]),
-                        // 楼层状况
-                        core.utils.data_builder.normal("floorInfo", ["li:eq(6) .des"]),
-                        // 工程进度
-                        core.utils.data_builder.normal("jobProgress", ["li:eq(7) .des"]),
-                        // 物业费
-                        core.utils.data_builder.normal("propertyFee", ["li:eq(8) .des"]),
-                        // 物业公司
-                        core.utils.data_builder.normal("propertyCompany", ["li:eq(9) .des"]),
-                        // 车位数
-                        core.utils.data_builder.normal("parkingAmount", ["li:eq(10) .des"]),
-                        // 车位比
-                        core.utils.data_builder.normal("parkingRate", ["li:eq(11) .des"], [".space"])
+                        core.utils.data_builder.switchs([], [], [
+                            core.utils.data_builder.cases(".name", [], "建筑类型", [
+                                core.utils.data_builder.normal("buildingType", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "产权年限", [
+                                core.utils.data_builder.normal("propertyAge", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "装修标准", [
+                                core.utils.data_builder.normal("decorationStandard", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "容积率", [
+                                core.utils.data_builder.normal("volumeRate", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "绿化率", [
+                                core.utils.data_builder.normal("greeningRate", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "规划户数", [
+                                core.utils.data_builder.normal("totalHouseholds", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "楼层状况", [
+                                core.utils.data_builder.normal("floorInfo", [".des"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "工程进度", [
+                                core.utils.data_builder.normal("jobProgress", [".des"]),
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "物业管理费", [
+                                core.utils.data_builder.normal("propertyFee", [".des"]),
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "物业公司", [
+                                core.utils.data_builder.normal("propertyCompany", [".des"]),
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "车位比", [
+                                core.utils.data_builder.normal("parkingAmount", [".des"]),
+                            ])
+                        ])
                     ]
                 },
                 moreInfo: {
-                    selector: ".can-container .can-item:eq(3) .can-border .list",
+                    selector: ".can-container .can-item:eq(3) .can-border .list li",
                     dealStrategy: "jsdom",
                     data: [
-                        // 最新动态
-                        core.utils.data_builder.normal("latestNews", ["li:eq(1) .des"], [".space"]),
-                        // 最新楼评
-                        core.utils.data_builder.normal("latestComment", ["li:eq(2) .des"], [".space"]),
-                        // 最新画报
-                        core.utils.data_builder.normal("latestPictorial", ["li:eq(3) .des"], [".space"]),
-                        // 最新导购
-                        core.utils.data_builder.normal("latestDynamic", ["li:eq(4) .des"], [".space"])
+                        core.utils.data_builder.switchs([], [], [
+                            core.utils.data_builder.cases(".name", [], "最新动态", [
+                                core.utils.data_builder.normal("latestNews", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "最新楼评", [
+                                core.utils.data_builder.normal("latestComment", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "最新画报", [
+                                core.utils.data_builder.normal("latestPictorial", [".des"], [".space"])
+                            ]),
+                            core.utils.data_builder.cases(".name", [], "最新导购", [
+                                core.utils.data_builder.normal("latestDynamic", [".des"], [".space"])
+                            ])
+                        ])
                     ]
                 }
             },
