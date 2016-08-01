42782532f0871a6fd58b76b42c68e1992f3866d2
nick <nick.feng@chinaredstar.com>
2016-08-01 11:14:16 +0800

    dfdf

--- core/config/production.js
+++ core/config/production.js
@@ -1,6 +1,6 @@
 module.exports = exports = {
     elastic: {
-        host: "10.25.255.50",
+        host: "114.55.146.215",
         port: 9200
     },
     q: {
--- tools/config/anjuke/index.js
+++ tools/config/anjuke/index.js
@@ -20,11 +20,11 @@ module.exports = (core) => {
     // 白名单
 
     // 楼盘列表、分页、相册,户型
-    config.addWhitePath(/^\/loupan(?:\/?|\/(?:[a-z]*(?:\/p\d+\/?|\/?))|\/(?:canshu|huxing|xiangce|)-?\d+\/?(?:ybj|sjt|xgt|ght|ptt|wzt|)\.html)$/);
+    // config.addWhitePath(/^\/loupan(?:\/?|\/(?:[a-z]*(?:\/p\d+\/?|\/?))|\/(?:canshu|huxing|xiangce|)-?\d+\/?(?:ybj|sjt|xgt|ght|ptt|wzt|)\.html)$/);
     // 匹配小区列表页，小区详情页,户型图页（只根据区域版块过滤）
     config.addWhitePath(/^\/community\/?(?:[a-z]*(?:\/p\d+\/?|\/?)|\/view\/\d+|photos\/model\/\d+)\/?$/);
     // 经纪人列表,经纪人详情,经纪人个人信息
-    config.addWhitePath(/^(?:\/tycoon\/?(?:[a-z]*(?:\/?p\d+|)|)|\/gongsi-jjr-\d+(\/js)?)\/?$/);
+    // config.addWhitePath(/^(?:\/tycoon\/?(?:[a-z]*(?:\/?p\d+|)|)|\/gongsi-jjr-\d+(\/js)?)\/?$/);
 
     _.forEach(core.config.anjuke.pages, (page) => {
         if (typeof page === "function") {
