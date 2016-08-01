d5b030421b92a5541b673ecef6967e5e91ebdc53
nick <nick.feng@chinaredstar.com>
2016-08-01 23:24:53 +0800

    dfadf

--- tools/chvps.js
+++ tools/chvps.js
@@ -8,20 +8,21 @@ let commands = [
     "service nginx restart",
     "service nginx stop",
     "service nginx start",
-    "route"
+    "route",
+    "route delete default gw all"
 ];
 let scheduleJob1 = () => {
     console.log("nginx restart at ", new Date());
-    shell.exec(commands[3], {silent: false});
+    shell.exec(commands[3], { silent: false });
 };
 let scheduleJob = () => {
     let isSuccess, localhostIp, pptpsetup, datas = [];
 
-    shell.exec(commands[4], {silent: false});
+    shell.exec(commands[4], { silent: false });
     console.log("poff restart at ", new Date());
-    shell.exec(commands[0], {silent: false});
+    shell.exec(commands[0], { silent: false });
     console.log("pptpsetup restart at ", new Date());
-    pptpsetup = shell.exec(commands[1], {silent: true, async: true});
+    pptpsetup = shell.exec(commands[1], { silent: true, async: true });
     pptpsetup.stdout.on("data", (data) => {
         !isSuccess && (isSuccess = /succeeded/i.test(data));
         datas.push(data);
@@ -33,16 +34,17 @@ let scheduleJob = () => {
                 console.log("ok");
                 console.log("route restart at ", new Date());
 
-                shell.exec(commands[2] + localhostIp[0], {silent: false});
+                shell.exec(commands[7], { silent: false });
+                shell.exec(commands[2] + localhostIp[0], { silent: false });
 
-                let route = shell.exec(commands[6], {silent: false}).stdout;
+                let route = shell.exec(commands[6], { silent: false }).stdout;
 
                 console.log(route);
                 console.log("ip是否在route中", route.indexOf(localhostIp[0]));
 
                 if (route.indexOf(localhostIp[0]) > 0) {
 
-                    shell.exec(commands[5], {silent: false});
+                    shell.exec(commands[5], { silent: false });
                     shell.exit(1);
                 }
 
