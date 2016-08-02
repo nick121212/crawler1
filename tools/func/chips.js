13cea184873a6f0e0817f88b2177c5ca0ea60fe9
nick <nick.feng@chinaredstar.com>
2016-08-02 18:05:20 +0800

    dfdfdfadf

--- tools/func/chips.js
+++ tools/func/chips.js
@@ -18,7 +18,6 @@ module.exports = exports = (core) => {
     let isRunning = false,
         retryCount = 0;
     let scheduleJob1 = () => {
-        console.log("nginx restart at ", new Date());
         shell.exec(commands[3], {silent: false});
         retryCount = 0;
         isRunning = false;
@@ -29,32 +28,25 @@ module.exports = exports = (core) => {
         if (isRunning) return;
         isRunning = true;
         shell.exec(commands[4], {silent: false});
-        console.log("poff restart at ", new Date());
         shell.exec(commands[0], {silent: false});
 
-        setTimeout(function () {
-            "use strict";
-
-            retryCount++;
-            console.log("pptpsetup restart at ", new Date());
-            pptpsetup = shell.exec(commands[1], {silent: true, async: true});
-            pptpsetup.stdout.on("data", (data) => {
-                !isSuccess && (isSuccess = /succeeded/i.test(data));
-                datas.push(data);
-
-                if (/remote/i.test(datas.join(""))) {
-                    isSuccess && (localhostIp = datas.join("").match(/([1-9]{1,3}\.){3}[1-9]{1,3}/ig));
-                    console.log(localhostIp);
-                    if (isSuccess && localhostIp.length > 1) {
-                        console.log("route restart at ", new Date());
+        retryCount++;
+        pptpsetup = shell.exec(commands[1], {silent: true, async: true});
+        pptpsetup.stdout.on("data", (data) => {
+            !isSuccess && (isSuccess = /succeeded/i.test(data));
+            datas.push(data);
+            if (/remote/i.test(datas.join(""))) {
+                isSuccess && (localhostIp = datas.join("").match(/([1-9]{1,3}\.){3}[1-9]{1,3}/ig));
+                if (isSuccess && localhostIp.length > 1) {
+                    setTimeout(()=> {
+                        "use strict";
                         shell.exec(commands[2] + localhostIp[0], {silent: false});
                         let route = shell.exec(commands[6], {silent: false}).stdout;
-                        console.log("ip是否在route中", route.indexOf(localhostIp[0]));
                         if (route.indexOf(localhostIp[0]) > 0) {
                             shell.exec(commands[5], {silent: false});
                             setTimeout(scheduleJob1, 5000);
                         } else {
-                            if (retryCount > 10) {
+                            if (retryCount > 5) {
                                 return shell.exit(1);
                             }
                             setTimeout(function () {
@@ -62,10 +54,10 @@ module.exports = exports = (core) => {
                                 scheduleJob();
                             }, 10);
                         }
-                    }
+                    }, 1000);
                 }
-            });
-        }, 1000);
+            }
+        });
     };
     return (options) => {
         schedule.scheduleJob(`*/${options.interval || 1} * * * *`, scheduleJob);
