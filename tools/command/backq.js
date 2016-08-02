ed7bb4caa04892000c11a85e9767cc19701f554a
nick <nick.feng@chinaredstar.com>
2016-08-02 16:53:19 +0800

    dfdfdfadf

--- tools/command/backq.js
+++ tools/command/backq.js
@@ -1,3 +1,15 @@
 /**
- * Created by NICK on 16/8/2.
+ * Created by NICK on 16/7/1.
  */
+
+module.exports = exports = (core, program) => {
+    let action = (key, options) => {
+        core.func.backq(key, options).then(process.exit, process.exit);
+    };
+
+    program
+        .command('backq <key>')
+        .option('-i, --interval <n>', '等待时间')
+        .description('回滚queue')
+        .action(action);
+};
--- tools/func/backq.js
+++ tools/func/backq.js
@@ -1,3 +1,48 @@
 /**
- * Created by NICK on 16/8/2.
+ * Created by NICK on 16/7/1.
  */
+module.exports = exports = (core) => {
+    return (key, options) => {
+        let search = (index, from = 0, size = 20) => {
+            let defer = Promise.defer(), total = 0;
+
+            core.q.getQueue(`crawler.urls.${key}`, {}).then((result) => {
+                core.elastic.search({
+                    index: index,
+                    type: "mqurls",
+                    scroll: '10s',
+                    search_type: 'scan',
+                    from: from,
+                    size: size
+                }, function getMoreUntilDone(error, response) {
+                    if (error) {
+                        console.log(error);
+                        return defer.reject(error);
+                    }
+
+                    response.hits.hits.forEach(function (res) {
+                        res = res._source;
+                        result.ch.publish("amq.topic", `${result.q.queue}.urls`, new Buffer(JSON.stringify(res)), {
+                            persistent: true
+                        });
+                    });
+                    total += response.hits.hits.length;
+                    console.log("scroll to:", total);
+                    if (response.hits.total !== total) {
+                        core.elastic.scroll({
+                            scrollId: response._scroll_id,
+                            scroll: '30s'
+                        }, getMoreUntilDone);
+                    } else {
+                        console.log("done");
+                        defer.resolve();
+                    }
+                });
+            }, defer.resolve);
+
+            return defer.promise;
+        };
+
+        return search(`${key}-crawler-allin`);
+    };
+};
