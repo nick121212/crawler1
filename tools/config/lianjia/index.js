5816aba523ee601fab8056fe006e5ebb48fb9f97
nick <nick121212@126.com>
2016-08-10 10:20:21 +0800

    dfdf

--- tools/config/lianjia/index.js
+++ tools/config/lianjia/index.js
@@ -10,7 +10,7 @@ module.exports = exports = (core) => {
     config.setBaseInfo(1000, "phantom1");
     config.initDomain = "sh.lianjia.com";
     config.proxySettings = {
-        useProxy: false && process.env.ENV == "production",
+        useProxy: true && process.env.ENV == "production",
         charset: "utf-8",
         timeout: 5000,
         // wait: 1000,
